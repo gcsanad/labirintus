f06e1299f8706fa99c26c13fd612732cd1550063
beketamas <72067154+beketamas@users.noreply.github.com>
2022-12-10 16:29:33 +0100

    még több cucc hozzá adva

--- labirirntusMakerBeta.cs
+++ labirirntusMakerBeta.cs
@@ -13,50 +13,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 		{
 
 			Random rnd = new Random();
-
-			List<char> falak = new List<char>() { '╬', '═', '╦', '╩', '║', '╣', '╠', '╗', '╝', '╚', '╔', '█' };
-
-
-
-			Console.WriteLine("Adjon meg a mátrix méretét (pl: '2:3'): ");
-			string kordinata = Console.ReadLine();
-
-			string[] ok = kordinata.Split(":");
-
-
-			int szam = Convert.ToInt32(ok[0]);
-			int szam2 = Convert.ToInt32(ok[1]);
-
-			palyaKeszites(szam, szam2);
-
-			Console.WriteLine("1. pálya szerkeztése");
-			Console.WriteLine("2. pálya mentése");
-			Console.WriteLine("3. pálya betöltése");
-			Console.WriteLine();
-			Console.WriteLine("Válasszon ki egy menü pontot");
-
-			int bekeres = Convert.ToInt32(Console.ReadLine());
-
-
-
-			if (bekeres == 1)
-			{
-				Console.Clear();
-				szerkeztes(palyaKeszites(szam, szam2), falak);
-			}
-			
-			else if (bekeres == 2)
-			{
-				mentes(palyaKeszites(szam,szam2));
-			}
-			
-			else if (bekeres == 3)
-			{
-				Console.WriteLine("adja meg a mentett pálya nevét: ");
-				string nev = Console.ReadLine();
-				szerkeztes(betoltes(nev), falak);
-			}
-			
+			menu();
 		}
 
 		static char[,] palyaKeszites(int sorokSzama, int sozlopokSzama)
@@ -103,6 +60,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 				Console.WriteLine("Ha elszertné menteni a pályát akkor csak nyomjon egy ENTER-t!");
 				Console.WriteLine("Ha minden objektumot ki szeretne törölni a pályáról, akkor írja be hogy 'ures'");
 				Console.WriteLine("Ha karaktert szeretne törölin akkor a kordináták után rakjon egy 't betűt' kettős ponttal elválasztva (pl: 2:3:t)");
+				Console.WriteLine("Ha vissza szertene menni a menu akkro írja be hogy 'menu'");
 				Console.ForegroundColor = ConsoleColor.White;
 				Console.WriteLine("Adjon meg egy kordinátát ahova a falakat, vagy egy szobát szeretné rakni (pl: 2:3): ");
 				bekeres = Console.ReadLine();
@@ -111,7 +69,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
 				if (bekeres == "")
 				{
 					mentes(palya);
-					break;
+					continue;
+				}
+
+				else if (bekeres == "menu")
+				{
+					Console.Clear();
+					menu();
 				}
 
 				else if (bekeres == "ures")
@@ -150,6 +114,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
 					{
 						palya[xKord - 1, yKord - 1] = JEL;
 					}
+
+				
+
 
 					else
 					{
@@ -252,6 +219,49 @@ namespace MyApp // Note: actual namespace depends on the project name.
 			return palya;
 		}
 
+
+		static void menu()
+		{
+			List<char> falak = new List<char>() { '╬', '═', '╦', '╩', '║', '╣', '╠', '╗', '╝', '╚', '╔', '█' };
+			int szam = 0;
+			int szam2 = 0;
+			Console.WriteLine("1. pálya létrehozása");
+			Console.WriteLine("2. pálya mentése");
+			Console.WriteLine("3. pálya betöltése");
+			Console.WriteLine();
+			Console.WriteLine("Válasszon ki egy menü pontot");
+
+			int bekeres = Convert.ToInt32(Console.ReadLine());
+
+
+
+			if (bekeres == 1)
+			{
+				Console.WriteLine("Adjon meg a mátrix méretét (pl: '2:3'): ");
+				string kordinata = Console.ReadLine();
+
+				string[] ok = kordinata.Split(":");
+
+
+				szam = Convert.ToInt32(ok[0]);
+				szam2 = Convert.ToInt32(ok[1]);
+
+				szerkeztes(palyaKeszites(szam, szam2), falak);
+			}
+
+
+			else if (bekeres == 2)
+			{
+				mentes(palyaKeszites(szam, szam2));
+			}
+
+			else if (bekeres == 3)
+			{
+				Console.WriteLine("Adja meg a mentett pálya nevét: ");
+				string nev = Console.ReadLine();
+				szerkeztes(betoltes(nev), falak);
+			}
+		}
 
 	}
 }
