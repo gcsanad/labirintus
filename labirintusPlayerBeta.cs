5fbc9ea5912d93b46db0a2bf92dae50ab4bab1fc
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-01-10 23:50:28 +0100

    Add files via upload

--- labirintusPlayerBeta.cs
+++ labirintusPlayerBeta.cs
@@ -5,71 +5,135 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
-        static char[,] Betoltes(string palyaNeve = "teszt.txt")
+        private static char[,] labirintus;
+        private static int jatekosSor, jatekosOszlop;
+        static void Main(string[] args)
         {
-            string[] megnyitas = File.ReadAllLines(palyaNeve);
-            char[,] labirintus = new char[megnyitas.Length, megnyitas[0].Length];
-            for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
-            {
-                for (int oszlopIndex = 0; oszlopIndex < labirintus.GetLength(1); oszlopIndex++)
-                {
-                    labirintus[sorIndex, oszlopIndex] = megnyitas[sorIndex][oszlopIndex];
+            // A fájl nevének a bekérése
+            //Console.Write("Írja ide a fájl nevét: ");
+            string palyaNeve = "teszt.txt";
 
+            // Pálya betöltése a fájlból
+            Betoltes(palyaNeve);
+
+            // Kezdési hely megkeresése
+            KezdopontKeresese();
+
+            // Pálya megjelenítése
+            Megjelenit();
+
+            // Folyamatosan kéri a felhasználótól a billentyűket és mozgatja a játékost
+            while (true)
+            {
+                
+                Console.Write("Merre mozog tovább (WASD): ");
+                char irany = Console.ReadKey().KeyChar;
+
+                // Játékos mozgatása
+                JatekosMozgatasa(irany);
+
+                // Pálya megjelenítése
+                Megjelenit();
+
+            }
+        }
+        private static void Betoltes(string palyaNeve = "teszt.txt")
+        {
+            // Beolvassa a fájl nevét és egy tömbben tárolja
+            string[] lines = File.ReadAllLines(palyaNeve);
+            labirintus = new char[lines.Length, lines[0].Length];
+
+            for (int sorIndex = 0; sorIndex < lines.Length; sorIndex++)
+            {
+                for (int oszlopIndex = 0; oszlopIndex < lines[sorIndex].Length; oszlopIndex++)
+                {
+                    labirintus[sorIndex, oszlopIndex] = lines[sorIndex][oszlopIndex];
+                    
                 }
             }
-
-            for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
+            for (int sorIndex = 0; sorIndex < lines.Length; sorIndex++)
             {
-                for (int oszlopIndex = 0; oszlopIndex < labirintus.GetLength(1); oszlopIndex++)
+                for (int oszlopIndex = 0; oszlopIndex < lines[sorIndex].Length; oszlopIndex++)
                 {
-                    Console.Write(labirintus[sorIndex, oszlopIndex]); 
-
+                    Console.Write(labirintus[sorIndex,oszlopIndex]);
 
                 }
                 Console.WriteLine();
             }
-
-            return labirintus;
         }
-
-        static void Main(string[] args)
+        private static void JatekosMozgatasa(char irany)
         {
-            Betoltes();
-            Console.SetCursorPosition(1, 0);
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.SetCursorPosition(5, 23);
-            char mozgas;
-            
-            do
+            int ujSor = jatekosSor;
+            int ujOszlop = jatekosOszlop;
+
+            // A játékos pozíciójának frissítése
+            switch (irany)
             {
-                Console.Clear();
-                Betoltes();
-                
-                Console.Write("Északra(w)\nKeletre(d)\nDélre(s)\nNyugatra(a)\n(K)ilépés");
-                mozgas = Console.ReadKey().KeyChar;
-                switch (mozgas)
-                {
-                    case 'w':
+                case 'W':
+                case 'w':
+                    ujSor--;
+                    break;
+                case 'A':
+                case 'a':
+                    ujOszlop--;
+                    break;
+                case 'S':
+                case 's':
+                    ujSor++;
+                    break;
+                case 'D':
+                case 'd':
+                    ujOszlop++;
+                    break;
+                default:
+                    break;
+            }
 
-                        break;
-                    case 'd':
-                        break;
-                    case 's':
-                        break;
-                    case 'a':
-                        break;
-                    case 'k':
-                        Environment.Exit(0);
-                        break;
-                }
-            } while (true);
-            
-
+            // Ellenőrzi hogy a játékos pályán belül van e
+            if (ujSor >= 0 && ujSor < labirintus.GetLength(0) && ujOszlop >= 0 && ujOszlop < labirintus.GetLength(1) && labirintus[ujSor, ujOszlop] != '.')
+            {
+                jatekosSor = ujSor;
+                jatekosOszlop = ujOszlop;
+            }
         }
-        
+        private static void Megjelenit()
+        {
+            Console.Clear();
+            for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
+            {
+                for (int oszlopIndex = 0; oszlopIndex < labirintus.GetLength(1); oszlopIndex++)
+                {
+                    if (sorIndex == jatekosSor && oszlopIndex == jatekosOszlop)
+                    {
+                        Console.Write('P');
+                    }
+                    else
+                    {
+                        Console.Write(labirintus[sorIndex, oszlopIndex]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void KezdopontKeresese()
+        {
+            for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
+            {
+                for (int oszlopIndex = 0; oszlopIndex < labirintus.GetLength(1); oszlopIndex++)
+                {
+                    if (labirintus[sorIndex, oszlopIndex] == '═')
+                    {
+                        jatekosSor = 1;
+                        jatekosOszlop = 0;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
