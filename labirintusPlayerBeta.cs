7dded6b854fa223b029c5a3114e240129bb292e1
gcsanad <71721197+gcsanad@users.noreply.github.com>
2023-01-11 22:48:14 +0100

    Add files via upload

--- labirintusPlayerBeta.cs
+++ labirintusPlayerBeta.cs
@@ -5,17 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApp2
+namespace ConsoleApp3
 {
     class Program
     {
-        private static char[,] labirintus;
-        private static int jatekosSor, jatekosOszlop;
+        static char[,] labirintus;
+        static int jatekosSor, jatekosOszlop;
         static void Main(string[] args)
         {
+            string[] cim = File.ReadAllLines("cim.txt");
+            foreach (string sor in cim)
+            {
+                Console.WriteLine(sor);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Nyomjon le egy billentyűt a folytatáshoz");
+            Console.ReadKey();
+            Console.Clear();
             // A fájl nevének a bekérése
-            //Console.Write("Írja ide a fájl nevét: ");
-            string palyaNeve = "teszt.txt";
+            Console.Write("Írja ide a fájl nevét: ");
+            string palyaNeve = Console.ReadLine();
 
             // Pálya betöltése a fájlból
             Betoltes(palyaNeve);
@@ -29,7 +39,7 @@ namespace ConsoleApp2
             // Folyamatosan kéri a felhasználótól a billentyűket és mozgatja a játékost
             while (true)
             {
-                
+
                 Console.Write("Merre mozog tovább (WASD): ");
                 char irany = Console.ReadKey().KeyChar;
 
@@ -41,8 +51,14 @@ namespace ConsoleApp2
 
             }
         }
-        private static void Betoltes(string palyaNeve = "teszt.txt")
+        static void Betoltes(string palyaNeve)
         {
+            // Ellenőrzi, hogy van e ilyen nevű fájl
+            if (!File.Exists(palyaNeve))
+            {
+                Console.WriteLine("Nincs ilyen nevű  fájl!");
+                return;
+            }
             // Beolvassa a fájl nevét és egy tömbben tárolja
             string[] lines = File.ReadAllLines(palyaNeve);
             labirintus = new char[lines.Length, lines[0].Length];
@@ -52,20 +68,20 @@ namespace ConsoleApp2
                 for (int oszlopIndex = 0; oszlopIndex < lines[sorIndex].Length; oszlopIndex++)
                 {
                     labirintus[sorIndex, oszlopIndex] = lines[sorIndex][oszlopIndex];
-                    
+
                 }
             }
             for (int sorIndex = 0; sorIndex < lines.Length; sorIndex++)
             {
                 for (int oszlopIndex = 0; oszlopIndex < lines[sorIndex].Length; oszlopIndex++)
                 {
-                    Console.Write(labirintus[sorIndex,oszlopIndex]);
+                    Console.Write(labirintus[sorIndex, oszlopIndex]);
 
                 }
                 Console.WriteLine();
             }
         }
-        private static void JatekosMozgatasa(char irany)
+        static void JatekosMozgatasa(char irany)
         {
             int ujSor = jatekosSor;
             int ujOszlop = jatekosOszlop;
@@ -89,6 +105,10 @@ namespace ConsoleApp2
                 case 'd':
                     ujOszlop++;
                     break;
+                case 'k':
+                    Console.WriteLine("\nKöszönjük, hogy játszott a játékkal!");
+                    Environment.Exit(1);
+                    break;
                 default:
                     break;
             }
@@ -99,8 +119,9 @@ namespace ConsoleApp2
                 jatekosSor = ujSor;
                 jatekosOszlop = ujOszlop;
             }
+
         }
-        private static void Megjelenit()
+        static void Megjelenit()
         {
             Console.Clear();
             for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
@@ -109,7 +130,7 @@ namespace ConsoleApp2
                 {
                     if (sorIndex == jatekosSor && oszlopIndex == jatekosOszlop)
                     {
-                        Console.Write('P');
+                        Console.Write('▒');
                     }
                     else
                     {
@@ -120,20 +141,47 @@ namespace ConsoleApp2
             }
         }
 
-        private static void KezdopontKeresese()
+        static void KezdopontKeresese()
         {
+            //'╬','═','╦','╩','║','╣','╠','╗','╝','╚', '╔'
             for (int sorIndex = 0; sorIndex < labirintus.GetLength(0); sorIndex++)
             {
                 for (int oszlopIndex = 0; oszlopIndex < labirintus.GetLength(1); oszlopIndex++)
                 {
-                    if (labirintus[sorIndex, oszlopIndex] == '═')
+                    if (labirintus[sorIndex, 0] == '═' || labirintus[sorIndex, 0] == '╬' || labirintus[sorIndex, 0] == '╦' || labirintus[sorIndex, 0] == '╩' ||
+                        labirintus[sorIndex, 0] == '╣' || labirintus[sorIndex, 0] == '╗' || labirintus[sorIndex, 0] == '╝')
                     {
-                        jatekosSor = 1;
+                        jatekosSor = sorIndex;
                         jatekosOszlop = 0;
-                        break;
+
+                    }
+                    else if (labirintus[sorIndex, labirintus.GetLength(1) - 1] == '═' || labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╬' ||
+                        labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╦' || labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╩' ||
+                        labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╠' || labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╚' ||
+                        labirintus[sorIndex, labirintus.GetLength(1) - 1] == '╔')
+                    {
+                        jatekosSor = sorIndex;
+                        jatekosOszlop = labirintus.GetLength(1) - 1;
+                    }
+                    else if (labirintus[0, oszlopIndex] == '║' || labirintus[0, oszlopIndex] == '╬' || labirintus[0, oszlopIndex] == '╠' || labirintus[0, oszlopIndex] == '╩' ||
+                        labirintus[0, oszlopIndex] == '╣' || labirintus[0, oszlopIndex] == '╚' || labirintus[0, oszlopIndex] == '╝')
+                    {
+                        jatekosSor = 0;
+                        jatekosOszlop = oszlopIndex;
+
+                    }
+                    else if (labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '║' || labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╬' ||
+                        labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╦' || labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╠' ||
+                        labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╣' || labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╗' ||
+                        labirintus[labirintus.GetLength(0) - 1, oszlopIndex] == '╔')
+                    {
+                        jatekosSor = labirintus.GetLength(0)-1;
+                        jatekosOszlop = oszlopIndex;
+
                     }
                 }
             }
+
         }
     }
 }
